f351cc212314600b4a2abeeba11b2b3ac22126f7
clearytv <dev@cleary.tv>
2024-11-23 10:13:45 +0000

    Finished Plasmic Base

--- apps/customer-portal/src/pages/index.tsx
+++ apps/customer-portal/src/pages/index.tsx
@@ -1,1 +1,11 @@
- 
+import React from 'react';
+import { PlasmicComponent } from '@plasmicapp/loader-nextjs';
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    document.title = "Forever Yours";
+  }, []);
+
+  return <PlasmicComponent component="Homepage" />;
+} 
